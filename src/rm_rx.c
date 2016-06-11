4fbf542e56ba7e64111c4ae1b9f24da07fd588d3
Piotr Gregor <piotrek.gregor@gmail.com>
2016-06-11 20:48:24 +0100

    77 0.1.2 #fix valgrindize test suite 3 with Memcheck
    Fix memory leaks in rm_rx_insert_nonoverlapping_ch_ch_ref_link

--- src/rm_rx.c
+++ src/rm_rx.c
@@ -11,93 +11,6 @@
 #include "rm_rx.h"
 
 
-/*
-int
-rm_rx_insert_nonoverlapping_ch_ch(FILE *f, const char *fname,
-		struct twhlist_head *h, uint32_t L,
-		int (*f_tx_ch_ch)(const struct rm_ch_ch *),
-        size_t limit, size_t *blocks_n)
-{
-    int         fd, res;
-    struct stat fs;
-    uint32_t	file_sz, read_left, read_now, read;
-    size_t      entries_n;
-    struct rm_ch_ch	*e;
-    unsigned char	*buf;
-
-    assert(f != NULL);
-    assert(fname != NULL);
-    assert(L > 0);
-
-    / get file size /
-    fd = fileno(f);
-    res = fstat(fd, &fs);
-    if (res != 0)
-    {
-        RM_LOG_PERR("Can't fstat file [%s]", fname);
-        return -1;
-    }
-    file_sz = fs.st_size;
-
-    / read L bytes chunks /
-    read_left = file_sz;
-    read_now = rm_min(L, read_left);
-    buf = malloc(read_now);
-    if (buf == NULL)
-    {
-        RM_LOG_ERR("Malloc failed, L [%u], "
-                "read_now [%u]", L, read_now);
-        return -2;
-    }
-
-    entries_n = 0;
-    do
-    {
-        read = fread(buf, 1, read_now, f);
-        if (read != read_now)
-        {
-            RM_LOG_PERR("Error reading file [%s]", fname);
-            free(buf);
-            return -3;
-        }
-        / alloc new table entry /
-        e = malloc(sizeof (*e));
-        if (e == NULL)	
-        {
-            RM_LOG_PERR("Can't allocate table "
-                    "entry, malloc failed");
-            return -4;
-        }
-
-        / compute checksums /
-        e->f_ch = rm_fast_check_block(buf, read);
-        rm_md5(buf, read, e->s_ch.data);
-
-        if (h != NULL)
-        {
-            / insert into hashtable, hashing fast checksum /
-            TWINIT_HLIST_NODE(&e->hlink);
-            twhash_add_bits(h, &e->hlink, e->f_ch,
-                                RM_NONOVERLAPPING_HASH_BITS);
-        }
-        entries_n++;
-
-        / tx checksums to remote A ? /
-        if (f_tx_ch_ch != NULL)
-        {
-            res = f_tx_ch_ch(e);
-            if (res < 0)
-                return -5;
-        }
-        read_left -= read;
-        read_now = rm_min(L, read_left);
-    } while (read_now > 0 && entries_n < limit);
-
-    if (blocks_n != NULL)
-        *blocks_n = entries_n;
-    return	0;
-}*/
-
 int
 rm_rx_f_tx_ch_ch_ref_1(const struct f_tx_ch_ch_ref_arg_1 arg)
 {
@@ -266,8 +179,7 @@ rm_rx_insert_nonoverlapping_ch_ch_array(FILE *f, const char *fname,
     buf = malloc(read_now);
     if (buf == NULL)
     {
-        RM_LOG_ERR("Malloc failed, L [%u], "
-                "read_now [%u]", L, read_now);
+        RM_LOG_ERR("Malloc failed, L [%u], read_now [%u]", L, read_now);
         return -2;
     }
 
@@ -342,8 +254,7 @@ rm_rx_insert_nonoverlapping_ch_ch_ref_link(FILE *f, const char *fname,
     buf = malloc(read_now);
     if (buf == NULL)
     {
-        RM_LOG_ERR("Malloc failed, L [%u], "
-                "read_now [%u]", L, read_now);
+        RM_LOG_ERR("Malloc failed, L [%u], read_now [%u]", L, read_now);
         return -2;
     }
 
@@ -361,8 +272,8 @@ rm_rx_insert_nonoverlapping_ch_ch_ref_link(FILE *f, const char *fname,
         e = malloc(sizeof (*e));
         if (e == NULL)	
         {
-            RM_LOG_PERR("Can't allocate list "
-                    "entry, malloc failed");
+            free(buf);
+            RM_LOG_PERR("Can't allocate list entry, malloc failed");
             return -4;
         }
 
@@ -382,6 +293,8 @@ rm_rx_insert_nonoverlapping_ch_ch_ref_link(FILE *f, const char *fname,
     } while (read_now > 0 && entries_n < limit);
 
     *blocks_n = entries_n;
+    free(buf);
+
     return	0;
 }
 /*
