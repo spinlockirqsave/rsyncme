21def244ff79e49cc56801b798748520569b70b8
Piotr Gregor <piotrek.gregor@gmail.com>
2016-06-30 12:55:23 +0100

    126 0.1.2 Valgrindize test suite 8
    Use flags --tool=memcheck --leak-check=full --show-leak-kinds=all

--- test/src/test_rm_main8.c
+++ test/src/test_rm_main8.c
@@ -1,12 +1,9 @@
-/*
- * @file        test_rm_main8.c
- * @brief       Execution of test suite #8
- *              (rm_tx_local_push).
+/* @file        test_rm_main8.c
+ * @brief       Execution of test suite #8 (rm_tx_local_push).
  * @author      Piotr Gregor <piotrek.gregor at gmail.com>
  * @version     0.1.2
  * @date        15 June 2016 10:45 PM
- * @copyright	LGPLv2.1
- */
+ * @copyright	LGPLv2.1 */
 
 
 #include "rm_defs.h"
