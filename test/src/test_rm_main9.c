ec2b38d2869d7e8322e84c307bbaaae914668716
Piotr Gregor <piotrek.gregor@gmail.com>
2016-06-30 13:00:26 +0100

    127 0.1.2 Valgrindize test suite 9
    Use flags --tool=memcheck --leak-check=full --show-leak-kinds=all

--- test/src/test_rm_main9.c
+++ test/src/test_rm_main9.c
@@ -1,23 +1,18 @@
-/*
- * @file        test_rm_main9.c
- * @brief       Execution of test suite #9
- *              (checking of local push error checking).
+/* @file        test_rm_main9.c
+ * @brief       Execution of test suite #9 (checking of local push error checking).
  * @author      Piotr Gregor <piotrek.gregor at gmail.com>
  * @version     0.1.2
  * @date        20 June 2016 04:47 PM
- * @copyright	LGPLv2.1
- */
+ * @copyright	LGPLv2.1 */
 
 
 #include "rm_defs.h"
 #include "test_rm9.h"
 
 
-int main(void)
-{
+int main(void) {
     const struct CMUnitTest tests[] = {
 	    cmocka_unit_test(test_rm_local_push_err_1)
     };
-    return cmocka_run_group_tests(tests,
-		test_rm_setup, test_rm_teardown);
+    return cmocka_run_group_tests(tests, test_rm_setup, test_rm_teardown);
 }
