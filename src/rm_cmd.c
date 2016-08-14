9e12ddd25a773b019b72688e66a126a2e3d7a17f
Piotr Gregor <piotrgregor@rsyncme.org>
2016-08-14 23:50:59 +0100

    192 0.1.2 Print thresholds fired info

--- src/rm_cmd.c
+++ src/rm_cmd.c
@@ -105,6 +105,15 @@ print_stats(struct rm_delta_reconstruct_ctx rec_ctx) {
     }
     fprintf(stderr, "\ntime        : real [%lf]s, cpu [%lf]s", real_time, cpu_time);
     fprintf(stderr, "\nbandwidth   : [%lf]MB/s\n", ((double) bytes / 1000000) / real_time);
+    if ((rec_ctx.copy_all_threshold_fired == 1) || (rec_ctx.copy_tail_threshold_fired == 1)) {
+        fprintf(stderr, "\n");
+    }
+    if (rec_ctx.copy_all_threshold_fired == 1) {
+        fprintf(stderr, "copy all    : fired\n");
+    }
+    if (rec_ctx.copy_tail_threshold_fired == 1) {
+        fprintf(stderr, "copy tail   : fired\n");
+    }
 }
 
 int
